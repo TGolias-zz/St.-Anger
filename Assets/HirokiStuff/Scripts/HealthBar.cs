bf8e0608d923cb81c3c4e8a21dac8f9f6c39abb5
Tyler Golias <tgolias94@gmail.com>
2015-08-14 03:53:36 -0400

    Clean up
    Forcing unity to format files in a source control friendly format.

---
+++ Assets/HirokiStuff/Scripts/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour{
+	
+	public Scrollbar healthBar;
+	public static int health = 100;
+	
+	public void Damage(int value)
+	{
+		health -= value;
+		
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown ("b")) {
+			Damage (10);
+		}
+		healthBar.size = (float) health / 100.0f;
+	}
+}
